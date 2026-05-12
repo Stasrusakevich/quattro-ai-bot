da6c9aa5524cbabac3ace9288b7639936ba16b39
Stasrusakevich <38654512+Stasrusakevich@users.noreply.github.com>
2026-05-12 16:58:15 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,6 +13,7 @@ import os
 from services.ai import generate_ai_response
 from services.logger import logger
 
+
 load_dotenv()
 
 TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
@@ -32,11 +33,22 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text(ai_response)
 
 
-app = ApplicationBuilder().token(TOKEN).build()
+def main():
+    if not TOKEN:
+        raise ValueError("Не найден TELEGRAM_BOT_TOKEN")
 
-app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    app = ApplicationBuilder().token(TOKEN).build()
 
-logger.info("Bot started")
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(
+        MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message)
+    )
 
-app.run_polling()
+    logger.info("Bot started")
+    print("Bot started...")
+
+    app.run_polling()
+
+
+if __name__ == "__main__":
+    main()
