ed05933fb8e94e18ad007cc89173777deb82194b
tvoyvaric <vegorova260@gmail.com>
2026-05-12 16:32:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
+from dotenv import load_dotenv
 import os
 
-TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+load_dotenv()
+
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///quattro_ai.db")
