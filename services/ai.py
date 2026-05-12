d2c05fa82aa317a7fd644852182ef5c40d214c86
tvoyvaric <vegorova260@gmail.com>
2026-05-12 16:33:28 +0300

    Update ai.py

--- services/ai.py
+++ services/ai.py
@@ -1,2 +1,22 @@
+from groq import Groq
+from config import GROQ_API_KEY
+
+client = Groq(api_key=GROQ_API_KEY)
+
+
 def generate_ai_response(text):
-    return f"AI response for: {text}"
+    chat_completion = client.chat.completions.create(
+        messages=[
+            {
+                "role": "system",
+                "content": "Ты AI Assistant компании Quattro Space."
+            },
+            {
+                "role": "user",
+                "content": text
+            }
+        ],
+        model="llama-3.1-8b-instant"
+    )
+
+    return chat_completion.choices[0].message.content
