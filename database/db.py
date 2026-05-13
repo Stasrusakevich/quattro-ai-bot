2b459f5a778b1d4bfd17fb7a1ae44475d40fef04
tvoyvaric <vegorova260@gmail.com>
2026-05-13 10:45:51 +0300

    Update db.py

--- database/db.py
+++ database/db.py
@@ -1,2 +1,25 @@
+import sqlite3
+
+
+DB_NAME = "quattro_ai.db"
+
+
 def connect_db():
-    return "Database connected"
+    return sqlite3.connect(DB_NAME)
+
+
+def init_db():
+    conn = connect_db()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS messages (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id TEXT,
+        role TEXT,
+        content TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
