6272b4f726bc50c50db2961d20ce25623088428e
tvoyvaric <vegorova260@gmail.com>
2026-05-14 12:59:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,9 @@
 from dotenv import load_dotenv
 import os
 
+
 load_dotenv()
 
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+ADMIN_USER_ID = os.getenv("ADMIN_USER_ID")
